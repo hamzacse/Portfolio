a3834af4e070d28b11ee9773b965bc7139e75fa0
Hamza <itshamzakhan2002@gmail.com>
2023-10-27 10:22:42 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Hamza",
+  title: "ROS2 developer & AI enthusiast",
+  email: "itshamzakhan2002@gmail.com",
+  gitHub: "https://github.com/hamzacse",
+  instagram: "https://www.instagram.com/hamzah._.khan/",
+  linkedIn: "https://www.linkedin.com/in/hamzacse/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "https://www.youtube.com/@hamzakhan4311",
 };
 
 const primaryColor = "#4E567E";
